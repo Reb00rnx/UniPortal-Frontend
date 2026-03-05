703b2d875edc7e7d8943f19cb89e3d54fb4d18c8
Patryk Krysztoforski <patryk.kr0043@gmail.com>
2026-03-05 20:48:49 +0100

    last commit

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class DashboardComponent implements OnInit {
-  userRole: string | null = null;
   isStudent = false;
   isTeacher = false;
 
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    this.userRole = this.authService.getUserRole();
     this.isStudent = this.authService.isStudent();
     this.isTeacher = this.authService.isTeacher();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text);
   }
 }
